cbd52b88d19f637fdedc0f5b35d5447c9889f6eb
BrunoMatosSilva <bmsdevfullstack@gmail.com>
2023-05-14 21:54:14 -0300

    fix: correct text section title

--- app/components/pages/home/known-techs/index.tsx
+++ app/components/pages/home/known-techs/index.tsx
@@ -5,7 +5,7 @@ import { TbBrandNextjs } from "react-icons/tb"
 export const KnowsTechs = () => {
   return (
     <section className="container py-16">
-      <SectionTitle subtitle="compoetências" title="Conhecimentos" />
+      <SectionTitle subtitle="competências" title="Conhecimentos" />
 
       <div className="grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3 mt-[60px]">
         {Array.from({ length: 14}).map((_, index) => (
